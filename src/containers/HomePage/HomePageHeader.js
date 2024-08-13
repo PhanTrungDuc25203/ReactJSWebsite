36e7a419c590d2b922a24e444a55ecfb2aa3e5f4
PhanTrungDuc <phantrungduc2522005@gmail.com>
2024-08-13 17:40:53 +0700

    thêm nhiều icon từ fontawesome, hoàn thành header và banner của homepage

--- src/containers/HomePage/HomePageHeader.js
+++ src/containers/HomePage/HomePageHeader.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './HomePageHeader.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { } from '@fortawesome/free-brands-svg-icons';
+import { } from '@fortawesome/fontawesome-free-webfonts';
+import { } from '@fortawesome/fontawesome-svg-core';
+import { } from '@fortawesome/free-regular-svg-icons';
+import { faTooth, faHeartPulse, faSuitcaseMedical, faStethoscope } from '@fortawesome/free-solid-svg-icons';
+import { } from '@fortawesome/free-solid-svg-icons';
+import { } from '@fortawesome/react-fontawesome';
+
 
 class HomePageHeader extends Component {
 
@@ -69,13 +78,13 @@ class HomePageHeader extends Component {
                     <nav>
                         <ul id="home-top-navigator">
                             <li>Trang chủ</li>
-                            <li><i class="far fa-hospital nav-icon"></i>Khám chuyên khoa</li>
-                            <li><i class="fas fa-phone-volume nav-icon"></i>Khám từ xa</li>
-                            <li><i class="fas fa-child nav-icon"></i>Khám tổng quát</li>
-                            <li><i class="fas fa-vial nav-icon"></i>Xét nghiệm y học</li>
-                            <li><i class="far fa-heart nav-icon"></i>Sức khỏe tinh thần</li>
-                            <li><i class="fas fa-tablets nav-icon"></i>Khám nha khoa</li>
-
+                            <li><i className="far fa-hospital nav-icon"></i>Khám chuyên khoa</li>
+                            <li><i className="fas fa-phone-volume nav-icon"></i>Khám từ xa</li>
+                            <li><i className="fas fa-child nav-icon"></i>Khám tổng quát</li>
+                            <li><i className="fas fa-vial nav-icon"></i>Xét nghiệm y học</li>
+                            <li><FontAwesomeIcon icon={faHeartPulse} className="nav-icon fontawesome-icon" />Sức khỏe tinh thần</li>
+                            {/* <li><i class="fas fa-tablets nav-icon"></i>Khám nha khoa</li> */}
+                            <li><FontAwesomeIcon icon={faTooth} className="nav-icon fontawesome-icon" />Khám nha khoa</li>
 
                             <div id="marker"></div>
                         </ul>
@@ -88,9 +97,20 @@ class HomePageHeader extends Component {
                             <div className="search"></div>
                             <div className="search-box-title">Nền tảng chăm sóc sức khỏe toàn diện</div>
                             <div className="search-box-content">Hãy để chúng tôi chăm sóc cho bạn</div>
-                            <marquee> A simple scrolling text.
-                            </marquee>
+
+                            {/* <div className="spacing"></div> */}
+                            <div className="quick-search-container">
+                                <div className="quick-search">
+                                    <div className="quick-search-icon-1"><FontAwesomeIcon icon={faSuitcaseMedical} /></div>
+                                    <div className="quick-search-content-1"><marquee>Con tôi bị dị ứng lâu dài, làm thế nào để chữa trị dứt điểm?</marquee></div>
+                                </div>
+                                <div className="quick-search">
+                                    <div className="quick-search-icon-2"><FontAwesomeIcon icon={faStethoscope} /></div>
+                                    <div className="quick-search-content-2"><marquee>Dạo này xảy ra rất nhiều những trường hợp mắc bệnh, bạch hầu, bác sĩ có lời khuyên gì cho mọi người để phòng tránh?</marquee></div>
+                                </div>
+                            </div>
                         </div>
+
                     </div>
                     <div className="home-page-banners"></div>
 
