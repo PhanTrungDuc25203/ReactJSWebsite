b037b6aee96d0aa700ac9f51d7d859bf61da7785
PhanTrungDuc <phantrungduc2522005@gmail.com>
2024-08-17 00:34:32 +0700

    css sắp xong slider, sắp xong specialty section

--- src/containers/HomePage/Section/SpecialtySection/SpecialtySection.js
+++ src/containers/HomePage/Section/SpecialtySection/SpecialtySection.js
@@ -12,37 +12,151 @@ import { } from '@fortawesome/react-fontawesome';
 import { FormattedMessage } from 'react-intl';
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
-// import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick-theme.css";
+import '../../../../assets/specialty-image/101739-viem-gan.png';
 
 class SpecialtySection extends Component {
 
+    SampleNextArrow(props) {
+        const { className, style, onClick } = props;
+        return (
+            <div
+                className={`${className} slider-button-next override`}
+                style={style}
+                onClick={onClick}
+            />
+        );
+    }
+
+    // Định nghĩa hàm SamplePrevArrow
+    SamplePrevArrow(props) {
+        const { className, style, onClick } = props;
+        return (
+            <div
+                className={`${className} slider-button-prev override`}
+                style={style}
+                onClick={onClick}
+            />
+        );
+    }
 
     render() {
         const settings = {
-            dots: true,
+            dots: false,
             infinite: true,
-            slidesToShow: 2,
-            slidesToScroll: 2,
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            nextArrow: <this.SampleNextArrow />,  // Sử dụng SampleNextArrow
+            prevArrow: <this.SamplePrevArrow />,  // Sử dụng SamplePrevArrow
+            // autoplay: true,
+            autoplaySpeed: 6000,
+            speed: 1000,
+            pauseOnHover: true,
         };
+
         return (
             <div className="specialty-section">
                 <div className="specialty-contents">
+                    <div className="specialty-section-title">
+                        Chuyên khoa
+                        <div className="more-detail-button"><a href="#">Xem thêm</a></div>
+                    </div>
+
                     <Slider {...settings}>
-                        <div>
-                            <h1>A</h1>
+                        <div className="item-content">
+                            <div className="item-of-slider">
+                                <div className="image-of-item-1 image-css"></div>
+                                <div className="item-content">Bệnh viêm gan</div>
+                            </div>
                         </div>
-                        <div>
-                            <h1>B</h1>
+
+                        <div className="item-content">
+                            <div className="item-of-slider">
+                                <div className="image-of-item-2 image-css"></div>
+                                <div className="item-content">Châm cứu</div>
+                            </div>
                         </div>
-                        <div>
-                            <h1>C</h1>
+                        <div className="item-content">
+                            <div className="item-of-slider">
+                                <div className="image-of-item-3 image-css"></div>
+                                <div className="item-content">Bệnh xương khớp</div>
+                            </div>
                         </div>
-                        <div>
-                            <h1>D</h1>
+                        <div className="item-content">
+                            <div className="item-of-slider">
+                                <div className="image-of-item-4 image-css"></div>
+                                <div className="item-content">Hệ miễn dịch</div>
+                            </div>
+                        </div>
+                        <div className="item-content">
+                            <div className="item-of-slider">
+                                <div className="image-of-item-5 image-css"></div>
+                                <div className="item-content">Cột sống</div>
+                            </div>
+                        </div>
+                        <div className="item-content">
+                            <div className="item-of-slider">
+                                <div className="image-of-item-6 image-css"></div>
+                                <div className="item-content">Da liễu</div>
+                            </div>
+                        </div>
+                        <div className="item-content">
+                            <div className="item-of-slider">
+                                <div className="image-of-item-7 image-css"></div>
+                                <div className="item-content">Nhi khoa</div>
+                            </div>
+                        </div>
+                        <div className="item-content">
+                            <div className="item-of-slider">
+                                <div className="image-of-item-8 image-css"></div>
+                                <div className="item-content">Sản phụ khoa</div>
+                            </div>
+                        </div>
+                        <div className="item-content">
+                            <div className="item-of-slider">
+                                <div className="image-of-item-9 image-css"></div>
+                                <div className="item-content">Siêu âm thai</div>
+                            </div>
+                        </div>
+                        <div className="item-content">
+                            <div className="item-of-slider">
+                                <div className="image-of-item-10 image-css"></div>
+                                <div className="item-content">Sức khỏe tinh thần</div>
+                            </div>
+                        </div>
+                        <div className="item-content">
+                            <div className="item-of-slider">
+                                <div className="image-of-item-11 image-css"></div>
+                                <div className="item-content">Tai mũi họng</div>
+                            </div>
+                        </div>
+                        <div className="item-content">
+                            <div className="item-of-slider">
+                                <div className="image-of-item-12 image-css"></div>
+                                <div className="item-content">Dạ dày</div>
+                            </div>
+                        </div>
+                        <div className="item-content">
+                            <div className="item-of-slider">
+                                <div className="image-of-item-13 image-css"></div>
+                                <div className="item-content">Tim mạch</div>
+                            </div>
+                        </div>
+                        <div className="item-content">
+                            <div className="item-of-slider">
+                                <div className="image-of-item-14 image-css"></div>
+                                <div className="item-content">Thần kinh khoa</div>
+                            </div>
+                        </div>
+                        <div className="item-content">
+                            <div className="item-of-slider">
+                                <div className="image-of-item-15 image-css"></div>
+                                <div className="item-content">Y học cổ truyền</div>
+                            </div>
                         </div>
                     </Slider>
                 </div>
-            </div>
+            </div >
         );
     }
 
