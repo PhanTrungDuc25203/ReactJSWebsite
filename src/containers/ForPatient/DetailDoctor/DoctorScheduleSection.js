d1720f148654e74363196454351a305febb6e38b
PhanTrungDuc <phantrungduc2522005@gmail.com>
2024-09-02 22:38:27 +0700

    hoàn thần phần hiển thị timeframe cho người dùng chọn

--- src/containers/ForPatient/DetailDoctor/DoctorScheduleSection.js
+++ src/containers/ForPatient/DetailDoctor/DoctorScheduleSection.js
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from 'react';
 import { connect } from "react-redux";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCalendarDays } from '@fortawesome/free-solid-svg-icons';
 import { LANGUAGES } from '../../../utils';
 import moment from 'moment';
 import localization from 'moment/locale/vi';
@@ -12,6 +13,7 @@ class DoctorScheduleSection extends Component {
         super(props);
         this.state = {
             availableDays: [],
+            allAvailableTimeframe: [],
         }
     }
 
@@ -26,13 +28,18 @@ class DoctorScheduleSection extends Component {
         }
     }
 
+    capitalizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
     buildSelectionOpion = (language) => {
         let tempDateArr = [];
         for (let i = 0; i < 7; i++) {
             let object = {};
 
             if (language === LANGUAGES.VI) {
-                object.label = moment(new Date()).add(i, 'days').format('dddd - DD/MM');
+                let labelVi = moment(new Date()).add(i, 'days').format('dddd - DD/MM');
+                object.label = this.capitalizeFirstLetter(labelVi);
             } else if (language === LANGUAGES.EN) {
                 object.label = moment(new Date()).add(i, 'days').locale('en').format('ddd - DD/MM');
             }
@@ -53,13 +60,21 @@ class DoctorScheduleSection extends Component {
             let date = event.target.value;
 
             let res = await getDoctorScheduleByDateService(doctorId, date);
-            // console.log("Check res get schedule: ", res);
+
+            let tempAllAvailableTimeframe = [];
+            if (res && res.errCode === 0) {
+                this.setState({
+                    allAvailableTimeframe: res.data ? res.data : [],
+                })
+            }
+            console.log("Check res get schedule: ", res);
         }
     }
 
     render() {
 
-        let { availableDays } = this.state;
+        let { availableDays, allAvailableTimeframe } = this.state;
+        let { language } = this.props;
 
         return (
             <React.Fragment>
@@ -81,6 +96,29 @@ class DoctorScheduleSection extends Component {
                         }
 
                     </select>
+                </div>
+                <div className="available-timeframe">
+                    <div className="section-title">
+                        <FontAwesomeIcon icon={faCalendarDays} className="calendar-icon" />
+                        THỜI GIAN KHÁM:
+                    </div>
+                    <div className="timeframe-selection">
+                        {allAvailableTimeframe && allAvailableTimeframe.length > 0 ?
+                            allAvailableTimeframe.map((item, index) => {
+                                let timeframe = language === LANGUAGES.VI ? item.timeTypeData.value_Vie : item.timeTypeData.value_Eng;
+                                return (
+                                    <button key={index} className="timeframe-button">{timeframe}</button>
+                                )
+                            })
+
+                            :
+
+                            <div className="sorry-text-because-noavailable-timeframe">
+                                Xin lỗi quý khách, bác sĩ hiện không còn trống lịch hôm nay!
+                                Vui lòng chọn một hôm khác.
+                            </div>
+                        }
+                    </div>
                 </div>
                 <div className="timeframe-in-one-day">
 
