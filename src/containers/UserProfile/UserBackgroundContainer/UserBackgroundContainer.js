b4020f9cb09d76ae0ed10261c65bbeaba4661463
PhanTrungDuc <phantrungduc2522005@gmail.com>
2024-09-14 17:03:57 +0700

    tạo menu chuyển tab cho trang userProfile

--- src/containers/UserProfile/UserBackgroundContainer/UserBackgroundContainer.js
+++ src/containers/UserProfile/UserBackgroundContainer/UserBackgroundContainer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './UserBackgroundContainer.scss'; // Đường dẫn đến file CSS
 import CustomScrollbars from '../../../components/CustomScrollbars';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
+import { faPenToSquare, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 const UserBackgroundContainer = ({ width = 400, height = 200, riverWidth = 10, curveHeight = 50, imageUrl }) => {
     const riverPath = `
@@ -22,7 +22,7 @@ const UserBackgroundContainer = ({ width = 400, height = 200, riverWidth = 10, c
                     </div>
                     <div className="infor-section">
                         <div className="name">Phan Trung Duc</div>
-                        <div className="email">phanpiscean@gmail.com</div>
+                        <div className="email"><FontAwesomeIcon icon={faEnvelope} className="envelope-icon"/>phanpiscean@gmail.com</div>
                         <div className="edit-profile"><button><FontAwesomeIcon icon={faPenToSquare} className="edit-icon" />Chỉnh sửa thông tin của bạn</button></div>
                     </div>
                 </div>
--- src/containers/UserProfile/UserProfile.js
+++ src/containers/UserProfile/UserProfile.js
@@ -3,10 +3,11 @@ import { connect } from "react-redux";
 import './UserProfile.scss';
 import HomeFooter from '../HomePage/HomeFooter/HomeFooter';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { LANGUAGES } from '../../utils';
 import CustomScrollbars from '../../components/CustomScrollbars';
 import _ from 'lodash';
+import { withRouter } from 'react-router';
 import * as actions from "../../store/actions";
 import { MoonLoader } from 'react-spinners';
 import UserBackgroundContainer from './UserBackgroundContainer/UserBackgroundContainer';
@@ -16,7 +17,10 @@ class UserProfile extends Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            personalProfileOpened: true,
+            appointmentOpened: false,
+            commentAboutDoctorsOpended: false,
+            returnToHomePage: false,
         }
     }
 
@@ -36,16 +40,78 @@ class UserProfile extends Component {
         this.props.history.push(`/login`);
     }
 
+    handleProfileTabClicked(whichClicked) {
+        if (whichClicked === 'personalProfileOpen') {
+            this.setState({
+                personalProfileOpened: true,
+                appointmentOpened: false,
+                commentAboutDoctorsOpended: false,
+                returnToHomePage: false,
+            })
+        }
+        if (whichClicked === 'appointmentOpen') {
+            this.setState({
+                personalProfileOpened: false,
+                appointmentOpened: true,
+                commentAboutDoctorsOpended: false,
+                returnToHomePage: false,
+            })
+        }
+        if (whichClicked === 'commentAboutDoctorsOpen') {
+            this.setState({
+                personalProfileOpened: false,
+                appointmentOpened: false,
+                commentAboutDoctorsOpended: true,
+                returnToHomePage: false,
+            })
+        }
+        if (whichClicked === 'returnToHomePage') {
+            this.setState({
+                personalProfileOpened: false,
+                appointmentOpened: false,
+                commentAboutDoctorsOpended: false,
+                returnToHomePage: true,
+            }, this.props.history.push(`/home`));
+        }
+    }
+
     render() {
+        let { personalProfileOpened, appointmentOpened, commentAboutDoctorsOpended, returnToHomePage } = this.state;
         return (
-            <React.Fragment>
+            <div className="user-profile-container">
                 <CustomScrollbars style={{ height: '100vh', width: '100%' }}>
                     <UserBackgroundContainer />
-                    This is user's profile page
-                    <button onClick={() => this.handleLoginForUser()}>Log out </button>
+                    <div className="content-container">
+                        <div className="nav-bar">
+                            <a href="#" onClick={() => this.handleProfileTabClicked('personalProfileOpen')} className={personalProfileOpened === true ? "active" : ""} >Trang cá nhân</a>
+                            <a href="#" onClick={() => this.handleProfileTabClicked('appointmentOpen')} className={appointmentOpened === true ? "active" : ""} >Lịch hẹn bác sĩ</a>
+                            <a href="#" onClick={() => this.handleProfileTabClicked('commentAboutDoctorsOpen')} className={commentAboutDoctorsOpended === true ? "active" : ""} >Nhận xét về bác sĩ</a>
+                            <a href="#" onClick={() => this.handleProfileTabClicked('returnToHomePage')} className={returnToHomePage === true ? "active" : ""} >Trang chủ</a>
+                        </div>
+                        {
+                            personalProfileOpened === true &&
+                            <div className="personal-profile">
+                                Personal profile
+                            </div>
+                        }
+                        {
+                            appointmentOpened === true &&
+                            <div className="personal-profile">
+                                Appoitment with doctor
+                            </div>
+                        }
+                        {
+                            commentAboutDoctorsOpended === true &&
+                            <div className="personal-profile">
+                                Your comments about doctors
+                            </div>
+                        }
+                        This is user's profile page
+                        <button onClick={() => this.handleLoginForUser()}>Log out </button>
+                    </div>
                     <HomeFooter />
                 </CustomScrollbars>
-            </React.Fragment >
+            </div >
 
         );
     }
@@ -65,4 +131,4 @@ const mapDispatchToProps = dispatch => {
     };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(UserProfile);
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(UserProfile));
