2f3e58d57cc3e54dcdd2b0bbbb2f456a6545d900
PhanTrungDuc <phantrungduc2522005@gmail.com>
2024-10-14 01:16:18 +0700

    tạo giao diện trang thông tin CSYT

--- src/containers/ForPatient/DetailMedicalFacility/DetailMedicalFacility.js
+++ src/containers/ForPatient/DetailMedicalFacility/DetailMedicalFacility.js
@@ -8,9 +8,10 @@ import { faMapLocationDot } from '@fortawesome/free-solid-svg-icons';
 import { getInfoOfMedicalFacility } from '../../../services/userService';
 import { LANGUAGES } from '../../../utils';
 import CustomScrollbars from '../../../components/CustomScrollbars';
-import DoctorScheduleComponent from '../DoctorScheduleComponent/DoctorScheduleComponent';
 import _ from 'lodash';
 import { MoonLoader } from 'react-spinners';
+import defaultMedicalFacility from '../../../assets/images/default-medical-facility-avatar-lite-1.jpg';
+import DoctorScheduleComponent from '../../ForPatient/DoctorScheduleComponent/DoctorScheduleComponent';
 
 class DetailMedicalFacility extends Component {
 
@@ -56,12 +57,47 @@ class DetailMedicalFacility extends Component {
 
     render() {
         console.log("check state: ", this.state.medicalFacility);
+        let { medicalFacility } = this.state;
+        let imageByBase64 = '';
+        if (medicalFacility && medicalFacility.image) {
+            imageByBase64 = Buffer.from(medicalFacility.image, 'base64').toString('binary');
+        }
         return (
             <React.Fragment>
                 <CustomScrollbars style={{ height: '100vh', width: '100%' }}>
                     <HomePageHeader isShowBanner={false} />
                     <div className="medical-facility-article-container">
-                        đây là trang web cho cơ sở y tế có id: {this.props.match.params.id}
+                        <div className="medical-facility-background-and-avatar-image">
+                            <div className="background-container">
+                                <div className="medical-facility-background"></div>
+                            </div>
+                            <div className="medical-facility-avatar-and-name">
+                                <div className={imageByBase64 ? "avatar-css" : "default-avatar-css"}
+                                    style={{ backgroundImage: `url(${imageByBase64 ? imageByBase64 : defaultMedicalFacility})` }}
+                                ></div>
+                                <div className="medical-facility-name-and-address">
+                                    <div className="medical-facility-name">
+                                        {medicalFacility && medicalFacility.name && medicalFacility.name}
+                                    </div>
+                                    <div className="medical-facility-address">
+                                        <FontAwesomeIcon className="location-icon" icon={faMapLocationDot} />
+                                        {medicalFacility && medicalFacility.provinceTypeDataForFacility && medicalFacility.provinceTypeDataForFacility.value_Vie && medicalFacility.provinceTypeDataForFacility.value_Vie}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="medical-facility-booking-with-doctor-section">
+                            {medicalFacility.medicalFacilityHaveSpecialtyAndDoctor && medicalFacility.medicalFacilityHaveSpecialtyAndDoctor.length > 0 &&
+                                medicalFacility.medicalFacilityHaveSpecialtyAndDoctor.map((item, index) => {
+                                    return (
+                                        <div className="doctors-of-this-medical-facility" key={index}>
+                                            <DoctorScheduleComponent doctorId={item.doctorId} />
+                                        </div>
+                                    )
+                                }
+                                )
+                            }
+                        </div>
                     </div>
                     <HomeFooter />
                 </CustomScrollbars>
