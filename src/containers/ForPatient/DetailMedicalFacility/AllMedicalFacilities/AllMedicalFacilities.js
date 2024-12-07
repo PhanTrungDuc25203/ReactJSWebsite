7986e4048b06b789ff7c55f6411dd3b58e8160ff
PhanTrungDuc <phantrungduc2522005@gmail.com>
2024-12-07 18:55:48 +0700

    làm xong trang hiển thị tất cả các cơ sở y tế và specialty

--- src/containers/ForPatient/DetailMedicalFacility/AllMedicalFacilities/AllMedicalFacilities.js
+++ src/containers/ForPatient/DetailMedicalFacility/AllMedicalFacilities/AllMedicalFacilities.js
@@ -1,55 +1,75 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import { connect } from "react-redux";
 import HomePageHeader from '../../../HomePage/HomePageHeader/HomePageHeader';
 import './AllMedicalFacilities.scss';
 import HomeFooter from '../../../HomePage/HomeFooter/HomeFooter';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMapLocationDot } from '@fortawesome/free-solid-svg-icons';
 import { getInfoOfMedicalFacility } from '../../../../services/userService';
-import { LANGUAGES } from '../../../../utils';
 import CustomScrollbars from '../../../../components/CustomScrollbars';
-import DoctorScheduleComponent from '../../DoctorScheduleComponent/DoctorScheduleComponent';
-import _ from 'lodash';
-import { MoonLoader } from 'react-spinners';
 
 class AllMedicalFacilities extends Component {
-
     constructor(props) {
         super(props);
         this.state = {
             allMedicalFacilityData: [],
-        }
+            currentPage: 1, // Trang hiện tại
+            itemsPerPage: 8, // Số item tối đa trên mỗi trang
+        };
     }
 
     async componentDidMount() {
         let res = await getInfoOfMedicalFacility('ALLANDIMAGEBUTSHORT');
         if (res.errCode === 0) {
+            const expandedData = res.infor.concat(res.infor, res.infor, res.infor, res.infor); // Nhân rộng dữ liệu
             this.setState({
-                allMedicalFacilityData: res.infor,
-            })
+                allMedicalFacilityData: expandedData,
+            });
         }
     }
 
-    componentDidUpdate(prevProps, prevState, snapshot) {
-
-    }
-
-    handleSpinnerTypeChange = (event) => {
-        this.setState({ spinnerType: event.target.value });
+    handlePageChange = (pageNumber) => {
+        this.setState({ currentPage: pageNumber });
     };
 
-    handleColorChange = (event) => {
-        this.setState({ color: event.target.value });
-    };
+    renderPagination = () => {
+        const { allMedicalFacilityData, itemsPerPage, currentPage } = this.state;
+        const totalPages = Math.ceil(allMedicalFacilityData.length / itemsPerPage);
 
-    handleSizeChange = (event) => {
-        this.setState({ size: parseInt(event.target.value, 10) });
+        return (
+            <div className="pagination-container">
+                <button
+                    onClick={() => this.handlePageChange(currentPage - 1)}
+                    disabled={currentPage === 1}
+                    className="pagination-button"
+                >
+                    Trang trước
+                </button>
+                {Array.from({ length: totalPages }, (_, index) => (
+                    <button
+                        key={index}
+                        onClick={() => this.handlePageChange(index + 1)}
+                        className={`pagination-button ${currentPage === index + 1 ? 'active' : ''}`}
+                    >
+                        {index + 1}
+                    </button>
+                ))}
+                <button
+                    onClick={() => this.handlePageChange(currentPage + 1)}
+                    disabled={currentPage === totalPages}
+                    className="pagination-button"
+                >
+                    Trang sau
+                </button>
+            </div>
+        );
     };
 
     render() {
+        const { allMedicalFacilityData, currentPage, itemsPerPage } = this.state;
 
-        let { language } = this.props;
-        let { allMedicalFacilityData } = this.state;
+        // Tính toán dữ liệu cho trang hiện tại
+        const indexOfLastItem = currentPage * itemsPerPage;
+        const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+        const currentData = allMedicalFacilityData.slice(indexOfFirstItem, indexOfLastItem);
 
         return (
             <React.Fragment>
@@ -59,43 +79,42 @@ class AllMedicalFacilities extends Component {
                         <div className="all-medical-facilities-container-title">
                             Bệnh viện & Cơ sở y tế
                         </div>
-                        {allMedicalFacilityData && allMedicalFacilityData.length > 0 &&
-                            allMedicalFacilityData.map((item, index) => {
+                        {currentData && currentData.length > 0 &&
+                            currentData.map((item, index) => {
+                                let imageByBase64 = '';
+                                if (item.image) {
+                                    imageByBase64 = Buffer.from(item.image, 'base64').toString('binary');
+                                }
                                 return (
-                                    <div className="medical-facility-item-container" key={index}
-                                    // onClick={() => this.handleViewDetailArticleOfAFacility(item.id)}
-                                    >
+                                    <div className="medical-facility-item-container" key={index}>
                                         <div className="medical-facility-item">
-                                            <div className="image image-css">
-
-                                            </div>
+                                            <div
+                                                className="image-css"
+                                                style={{
+                                                    backgroundImage: `url(${imageByBase64})`,
+                                                }}
+                                            ></div>
                                             <div className="medical-facility-name">
                                                 {item.name}
                                             </div>
                                         </div>
                                     </div>
-                                )
+                                );
                             })
                         }
                     </div>
+                    {this.renderPagination()}
                     <HomeFooter />
                 </CustomScrollbars>
-            </React.Fragment >
-
+            </React.Fragment>
         );
     }
 }
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
     return {
         language: state.app.language,
     };
 };
 
-const mapDispatchToProps = dispatch => {
-    return {
-
-    };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(AllMedicalFacilities);
+export default connect(mapStateToProps)(AllMedicalFacilities);
