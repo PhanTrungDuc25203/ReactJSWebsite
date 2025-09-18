47e880491749a74f4ade3749f6c13dccff505c48
PhanTrungDuc25203 <phantrungduc2522005@gmail.com>
2025-09-18 15:29:21 +0700

    UI: css search bar in search page

--- src/containers/ForPatient/SeachPage/SearchPage.js
+++ src/containers/ForPatient/SeachPage/SearchPage.js
@@ -1,10 +1,20 @@
 import { connect } from "react-redux";
 import React, { Component } from "react";
+import "./SearchPage.scss";
+import HomePageHeader from "../../HomePage/HomePageHeader/HomePageHeader";
+import CustomScrollbars from "../../../components/CustomScrollbars";
+import HomeFooter from "../../../containers/HomePage/HomeFooter/HomeFooter";
 
 class SearchPage extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      specialtyList: [],
+      medicalFacilityList: [],
+      doctorList: [],
+      packageList: [],
+      searchTerm: "",
+    };
   }
 
   componentDidMount() {}
@@ -12,7 +22,24 @@ class SearchPage extends Component {
   componentDidUpdate(prevProps, prevState, snapshot) {}
 
   render() {
-    return <div className="search-page-component">This is search page</div>;
+    return (
+      <CustomScrollbars style={{ height: "100vh", width: "100%" }}>
+        <HomePageHeader isShowBanner={false} />
+        <div className="search-page-container">
+          <div className="search-bar-container">
+            <div className="search-bar">
+              <input type="text" placeholder="Tìm kiếm..."></input>
+              <select id="basic-select">
+                <option value="all">Tất cả</option>
+                <option value="name">Theo tên</option>
+                <option value="category">Theo danh mục</option>
+              </select>
+            </div>
+          </div>
+        </div>
+        <HomeFooter />
+      </CustomScrollbars>
+    );
   }
 }
 
